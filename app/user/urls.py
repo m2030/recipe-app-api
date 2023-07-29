d68df79ae204d9e952e8495135d64e5f73ab58ac
m2030 <mohamed.abouhasem@hotmail.com>
2023-07-29 12:40:06 +0300

    adding regular user API

--- app/user/urls.py
+++ app/user/urls.py
@@ -9,4 +9,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreaeTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
